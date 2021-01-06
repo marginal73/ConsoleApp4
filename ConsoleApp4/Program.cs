49d15011b39e02112c64012012868603f655238e
Name <email@example.ru>
2021-01-06 20:14:50 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string MyName
+            MyName = "Andrey"
+
+            Console.WriteLine(MyName);
+
+            Console.ReadKey();
+        }
+    }
+}
